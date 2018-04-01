5fbc7443d7e5a1339b6702876fa84530c2104fab
Marianne Kelly <marianne_kelly@hotmail.com>
2018-04-01 13:22:18 -0400

    fish option component images loaded

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -14,18 +16,28 @@ class App extends React.Component {
     //add new fish to fish var
     fishes[`fish${Date.now()}`] = fish;
     //set new fishes object to state
-    this.setState({
-      fishes
-    });
+    this.setState({ fishes });
     console.log('adding a fish')
+  };
+
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
   }
+
 render() {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline="Fresh Seafood Market" />
+          <ul className='fishes'>
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]}/>
+            ))}
+          </ul>
         </div>
-         <Inventory addFish={this.addFish} />
+         <Inventory addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       <Order />
       </div>
     );
